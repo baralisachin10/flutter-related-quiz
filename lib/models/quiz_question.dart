edc281d13cb4d403269b9579a1ccf37cfe6992b1
sacheen <baralisachin@gmail.com>
2023-06-10 17:53:27 +0545

    quiz app

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -2,4 +2,10 @@ class QuizQuestion {
   const QuizQuestion(this.questions, this.answers);
   final String questions;
   final List<String> answers;
+
+  List<String> getShuffledAnswer() {
+    final shuffledAnswer = List.of(answers);
+    shuffledAnswer.shuffle();
+    return shuffledAnswer;
+  }
 }
--- lib/questions.dart
+++ lib/questions.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/dummy_data/dummy_data.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -9,23 +11,48 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'The questions',
-            style: TextStyle(color: Colors.white),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          AnswerButton('Answer 1', () {}),
-          AnswerButton('Answer 2', () {}),
-          AnswerButton('Answer 3', () {}),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              textAlign: TextAlign.center,
+              currentQuestion.questions,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 222, 174, 234),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ...currentQuestion.getShuffledAnswer().map((item) {
+              return AnswerButton(
+                answerText: item,
+                onTap: answerQuestion,
+              );
+            }),
+            // AnswerButton(currentQuestion.answers[0], () {}),
+            // AnswerButton(currentQuestion.answers[1], () {}),
+            // AnswerButton(currentQuestion.answers[2], () {}),
+            // AnswerButton(currentQuestion.answers[3], () {}),
+          ],
+        ),
       ),
     );
   }
